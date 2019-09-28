1b86823f6e0d1eab4daf92032fa269252ee647ff
AJ <amarchitelli1993@gmail.com>
2019-09-28 18:14:02 -0400

    bold, colors, erase functions

--- src/components/Bracket.js
+++ src/components/Bracket.js
@@ -3,12 +3,33 @@ import React, { Component } from 'react'
 export default class Bracket extends Component {
 
   state = {
-    painting: false
+    painting: false,
+    bold: false,
+    color: 'black'
   }
 
+  boldButtonHandle = (event) => {
+    this.setState({
+      bold: !this.state.bold
+    })
+  }
+
+  colorHandle = (event) => {
+    this.setState({
+      color: event.target.name
+    })
+  }
+
+  eraseHandle = (event) => {
+    this.setState({
+      color: 'white'
+    })
+  }
+
+
   startPosition = (event) => {
-    const canvas = this.refs.canvas
-    const ctx = canvas.getContext("2d")
+    //const canvas = this.refs.canvas
+    //const ctx = canvas.getContext("2d")
 
     this.setState({
       painting: true
@@ -22,7 +43,8 @@ export default class Bracket extends Component {
     const ctx = canvas.getContext("2d")
     
     this.setState({
-      painting: false
+      painting: false,
+
     })
     ctx.beginPath()
   }
@@ -33,8 +55,11 @@ export default class Bracket extends Component {
     const ctx = canvas.getContext("2d")
 
     if(this.state.painting) {
-      ctx.lineWidth = 5
+      //depending on the state, the lineWidth is either 1 or 5..aka bold or not bold
+      this.state.bold===true ? ctx.lineWidth = 5 : ctx.lineWidth=1;
+      if(this.state.color==='white'){ctx.lineWidth = 20};
       ctx.lineCap = "round"
+      ctx.strokeStyle = this.state.color
 
       const rectangle = canvas.getBoundingClientRect()
       const height = event.clientY-rectangle.top
@@ -51,9 +76,8 @@ export default class Bracket extends Component {
 
   componentDidMount(){
     const canvas = this.refs.canvas
-    const ctx = canvas.getContext("2d")
+    //const ctx = canvas.getContext("2d")
    
-
     canvas.height = window.innerHeight
     canvas.width = window.innerWidth
   }
@@ -61,11 +85,22 @@ export default class Bracket extends Component {
   render() {
     return (
       <div>
-        <canvas onMouseDown={this.startPosition} 
+        <canvas 
+        onMouseDown={this.startPosition} 
         onMouseUp={this.finishPosition} 
         onMouseMove={this.draw} 
+
         ref="canvas" 
         />
+        <button onClick={this.boldButtonHandle}>bold</button>
+        <button name='black' onClick={this.colorHandle}>black</button>
+        <button name='red' onClick={this.colorHandle}>red</button>
+        <button name='blue' onClick={this.colorHandle}>blue</button>
+        <button name='green' onClick={this.colorHandle}>green</button>
+        <button name='yellow' onClick={this.colorHandle}>yellow</button>
+        <button name='purple' onClick={this.colorHandle}>purple</button>
+        <button onClick={this.eraseHandle}>eraser</button>
+
       </div>
     )
   }
